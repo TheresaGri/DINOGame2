68ecf0174d010afa6a603b98d36dda0cfa5c788a
kchristoph30 <c.kimi@hotmail.com>
2022-12-06 19:24:21 +0100

    Add Score

--- dino-game/script.js
+++ dino-game/script.js
@@ -1,36 +1,50 @@
-const dino = document.querySelector('#dino');
-const cactus = document.querySelector('#cactus');
+const dino = document.querySelector("#dino");
+const cactus = document.querySelector("#cactus");
+const counterElement = document.querySelector("#counter");
 
-document.addEventListener('keydown', event => {
-    jump(event);
-    move();
+document.addEventListener("keydown", (event) => {
+  jump(event);
+  move();
 });
 
 function jump(event) {
-    if (!(event.code === 'Space')) {
-        return;
-    }
-    if (!dino.classList.contains('jump')) {
-        dino.classList.add('jump');
-        setTimeout(function() {dino.classList.remove('jump')}, 1000);
-    }
+  if (!(event.code === "Space")) {
+    return;
+  }
+  if (!dino.classList.contains("jump")) {
+    dino.classList.add("jump");
+    setTimeout(function () {
+      dino.classList.remove("jump");
+    }, 1000);
+  }
 }
 
 function move() {
-    if (!cactus.classList.contains('move')) {
-        cactus.classList.add('move');
-    }
+  if (!cactus.classList.contains("move")) {
+    cactus.classList.add("move");
+  }
 }
 setInterval(() => {
-    if (isCrashed()) {
-        alert('Game Over!');
-    } 
-})
+  if (isCrashed()) {
+    alert("Game Over!");
+    counterElement.innerHTML = 0;
+  }
+});
 
 function isCrashed() {
-    let dinoPosition = dino.getBoundingClientRect();
-    let cactusPosition = cactus.getBoundingClientRect();
+  let dinoPosition = dino.getBoundingClientRect();
+  let cactusPosition = cactus.getBoundingClientRect();
 
-    return dinoPosition.right > cactusPosition.left && dinoPosition.left < cactusPosition.right && dinoPosition.bottom > cactusPosition.top;
+  return (
+    dinoPosition.right > cactusPosition.left &&
+    dinoPosition.left < cactusPosition.right &&
+    dinoPosition.bottom > cactusPosition.top
+  );
 }
 
+let toAdd = 1;
+let ms = 10 * 60;
+setInterval(() => {
+  var count = parseFloat(counterElement.innerHTML);
+  counterElement.innerHTML = !isNaN(count) == true ? count + toAdd : toAdd;
+}, ms);
